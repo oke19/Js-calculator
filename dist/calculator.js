c589faa0c7667c1b9554056069b8fbb1b732debd
Okechukwu <ezekielpromzyokechukwu@gmail.com>
2024-08-31 13:38:26 +0100

    initial commit

---
+++ dist/calculator.js
@@ -0,0 +1,24 @@
+//CALCULATOR PROGRAM
+
+const display = document.getElementById("display");
+
+function appendToDisplay(value) {
+    display.value += value;
+
+}
+
+
+function clearDisplay(){
+    display.value = "";
+
+}
+
+function calculateResult(){
+    try {
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "ERROR!😦";
+    }
+
+}
